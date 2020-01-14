e5d974f5a3ca80bf07416c6a0da95dad96c846b9
Glitch (lion-music) <none>
2020-01-14 18:26:38 +0000

    🏠☃️ Checkpoint ./README.md:3581793/641 ./ayarlar.json:3581793/18 ./package.json:3581793/2791 ./util/eventLoader.js:3581793/877 ./events/ready.js:3581793/877 ./start.js:3581793/57

--- events/ready.js
+++ events/ready.js
@@ -19,13 +19,4 @@ module.exports = client => {
   //PLAYING = OYNUYOR 
   console.log(`${client.user.username}: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
-/*
-__   __ _ ______     ______                     _                             _____                         
-\ \ / /(_)| ___ \    |  _  \                   | |                           |_   _|                        
- \ V /  _ | |_/ /    | | | |  ___ __   __  ___ | |  ___   _ __    ___  _ __    | |    ___   __ _  _ __ ___  
- /   \ | ||    /     | | | | / _ \\ \ / / / _ \| | / _ \ | '_ \  / _ \| '__|   | |   / _ \ / _` || '_ ` _ \ 
-/ /^\ \| || |\ \     | |/ / |  __/ \ V / |  __/| || (_) || |_) ||  __/| |      | |  |  __/| (_| || | | | | |
-\/   \/|_|\_| \_|    |___/   \___|  \_/   \___||_| \___/ | .__/  \___||_|      \_/   \___| \__,_||_| |_| |_|
-                                                         | |                                                
-                                                         |_|                                                
-*/
+

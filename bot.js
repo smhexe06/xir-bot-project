b65c2dfd02bb84442280821b6c94fb1c33fcc492
Glitch (lion-music) <none>
2020-06-09 16:56:12 +0000

    🏘👫 Checkpoint ./bot.js:15880089/436

--- bot.js
+++ bot.js
@@ -1,3 +1,19 @@
+//7/24
+{
+  const express = require("express");
+  const app = express();
+  const http = require("http");
+  app.get("/", (request, response) => {
+    console.log(
+      ` az önce pinglenmedi. Sonra ponglanmadı... ya da başka bir şeyler olmadı.`
+    );
+    response.sendStatus(200);
+  });
+  app.listen(process.env.PORT);
+  setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  }, 280000);
+} 
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const ayarlar = require("./ayarlar.json");

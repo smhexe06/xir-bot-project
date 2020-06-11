88a08b583b065b8631f82c8d93e33b92da22d08d
Glitch (seasoned-languid-teller-deleted) <none>
2020-06-11 18:51:50 +0000

    🍣🐑 Checkpoint ./komutlar/muzik-devamet.js:15880089/78 ./komutlar/muzik-duraklat.js:15880089/142 ./komutlar/muzik-durdur.js:15880089/142 ./komutlar/muzik-geç.js:15880089/142 ./komutlar/muzik-oynat.js:15880089/142 ./komutlar/muzik-ses.js:15880089/142 ./komutlar/muzik-tekrar.js:15880089/142

--- komutlar/muzik-tekrar.js
+++ komutlar/muzik-tekrar.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const { RichEmbed } = require('discord.js');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
-const youtube = new YouTube('AIzaSyBHp_5kYzIvI7Cd9tucCcJgL9VoaNUnQqY');
+const youtube = new YouTube('AIzaSyCM226IndbEeeFz_VGm1ASREp0t71TVKAA');
 
 exports.run = async (client, message, args) => {
         const queue = client.queue;
